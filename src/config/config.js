79890745a6e0e0692861bb0c4a0e28d81bbb73ff
David Castillo <cdavidrigoberto@gmail.com>
2024-08-08 08:46:50 -0600

    Añadiendo funcionalidades extras

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,8 @@ const DATABASE = process.env.DATABASE || 'ejercicios'
 const USER = process.env.USER || 'root'
 const PASSWORD = process.env.PASSWORD || ''
 const HOST = process.env.HOST || 'localhost'
+const MYSQL_PUBLIC_URL = process.env.MYSQL_PUBLIC_URL || 'http://localhost'
 
 module.exports = {
-    PORT,DATABASE,USER,PASSWORD, HOST
+    PORT,DATABASE,USER,PASSWORD, HOST,MYSQL_PUBLIC_URL
 }
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 const {Sequelize} = require('sequelize');
-const { DATABASE,USER,PASSWORD,HOST } = require('./config');
+const { DATABASE,USER,PASSWORD,HOST,MYSQL_PUBLIC_URL } = require('./config');
+
+const databaseUrl = MYSQL_PUBLIC_URL ||'fallback_connection_string'
+
 //configuramos los ajustes de la base de datos
-const sequelize = new Sequelize(DATABASE,USER,PASSWORD,{
+const sequelize = new Sequelize(databaseUrl,{
     host: HOST,
     dialect:'mysql',
     logging: false
