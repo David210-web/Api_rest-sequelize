881079f86ce01ced65a6a246febff153b7a6315e
David Castillo <cdavidrigoberto@gmail.com>
2024-08-08 08:48:22 -0600

    Haciendo cambios

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
 const {Sequelize} = require('sequelize');
-const { DATABASE,USER,PASSWORD,HOST,MYSQL_PUBLIC_URL } = require('./config');
-
-const databaseUrl = MYSQL_PUBLIC_URL ||'fallback_connection_string'
-
+const { DATABASE,USER,PASSWORD,HOST } = require('./config');
 //configuramos los ajustes de la base de datos
-const sequelize = new Sequelize(databaseUrl,{
+const sequelize = new Sequelize(DATABASE,USER,PASSWORD,{
     host: HOST,
     dialect:'mysql',
     logging: false
