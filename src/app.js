efb7c9b0d6974bd41e20165494904fabc34a3987
David Castillo <cdavidrigoberto@gmail.com>
2024-08-08 18:45:56 -0600

    Añadiendo cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const projectsRoutes = require('./routes/Projects.routes.js')
 const tasksRoutes = require('./routes/Tasks.routes.js')
 
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/projects',projectsRoutes)
 app.use('/api/tasks',tasksRoutes)
